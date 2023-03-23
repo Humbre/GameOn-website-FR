3982f3e85b4831effe04d64cb61b88da5bda3d65
humbre <leila.roserosette@outlook.fr>
2023-03-23 13:13:00 +0100

    rajouts de commentaires, dernières modifs 23/03

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,7 @@ function editNav() {
 
 // constantes du dom
 const modalbg = document.querySelector(".bground"); //background
-const modalBtn = document.getElementsByClassName(".btn-signup"); // deux boutons je m'inscris
+const modalBtn = document.getElementsByClassName(".btn-signup"); // deux boutons je m'inscris (desktop + responsive)
 const formData = document.querySelector(".formData"); // formulaire
 const closeBtn = document.querySelector(".close"); // croix pour fermer la modale
 const btnSubmit = document.querySelector(".btn-submit"); // bouton envoyer les données
@@ -23,12 +23,11 @@ const expressionReguliereMail = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+)
 const quantityRegex = /^(?:0|[1-9]\d{0,2})$/; // regex quantité
 
 
-// evenement + fonction pour ouvrir le formulaire (sur le bouton je m'inscris)
-
+// evenement dans boucle pour que le bouton je m'inscris fonctionne en responsive 
 for (i=0, i<modalBtn.length, i++){
   modalBtn[i].addEventListener("click", launchModal);
 }
-
+// + fonction pour ouvrir le formulaire (sur le bouton je m'inscris)
 function launchModal(){
   modalbg.style.display = "block";
   modalbg.style.opacity = "1";
@@ -36,21 +35,21 @@ function launchModal(){
 
 
 
-// evenement + fonction pour fermer le formulaire (avec la croix)
+// evenement pour fermer la modale
 closeBtn.addEventListener("click", closeModal);
-
+//+ fonction pour fermer le formulaire (avec la croix)
 function closeModal(){
   modalbg.style.display = "none";
 }
 
-// evenement + fonction pour envoyer les données si pas d'erreurs + reset
+// evenement pour envoyer les données 
 btnSubmit.addEventListener("click", sendData);
-
+//+ fonction pour envoyer les données si pas d'erreurs + reset
 function sendData(){
-
   let errors = validateData(); 
   showErrors(errors);
 
+  //condition d'affichage des erreurs
   if(Object.keys(errors).length > 0){ 
     alert('Il y a eu une erreur, veuillez réessayer');
     modalbg.style.display = "block";
@@ -69,74 +68,74 @@ function sendData(){
 function validateData(){
   let errors = {}; // erreurs stockées dans objet
 
-  if (firstName.value.length <2){ 
+  if (firstName.value.length <2){ //on verifie la longueur du prenom
     errors.firstName = "Votre prénom doit faire minimum 2 caractères";
   }
-  if (lastName.value.length <2){
+  if (lastName.value.length <2){ //On verifie la longueur du nom
     errors.lastName = "Votre nom doit faire minimum 2 caractères";
   }
-  if (!expressionReguliereMail.test(email.value)){
+  if (!expressionReguliereMail.test(email.value)){ // on verifie l'email avec la regex
     errors.email = "Veuillez entrer une adresse email valide";   
   }
-  if (!birthDate.value){
+  if (!birthDate.value){ // On verifie la date de naissance
     errors.birthDate = "Veuillez entrer votre date de naissance";
   }
-  if (!quantityRegex.test(quantity.value)){
+  if (!quantityRegex.test(quantity.value)){ // On verifie la quantité grace à la regex quantité
     errors.quantity = "Veuillez entrer un nombre entre 0 et 999";
   }
-  if (!document.querySelector('.checkbox-input1:checked')) {
+  if (!document.querySelector('.checkbox-input1:checked')) { // On verifie qu'une checkbox soit cochée dans les villes
     errors.checkBox = "Veuillez cocher une des cases précédentes";
   }
-  if (!document.querySelector('.checkbox-input2:checked')) {
+  if (!document.querySelector('.checkbox-input2:checked')) { // on verifie que les conditions d'utilisation soient cochées
     errors.checkBox1 = "Veuillez accepter les condition d'utilisation";
   }
-  return errors;
+  return errors; // On retourne les erreurs dans l'objet si erreur il y a
 }
 
 
 // fonction d'affichage de données si erreurs + reset de <p> si pas d'erreurs
 function showErrors(errors){
 
-  if (errors.firstName) {
-    document.getElementById("firstNameError").innerHTML = errors.firstName;
+  if (errors.firstName) { // S'il y a des erreurs dans le prenom, on affiche le texte, sinon on affiche rien
+    document.getElementById("firstNameError").innerText = errors.firstName;
   } else{
-    document.getElementById("firstNameError").innerHTML = "";
+    document.getElementById("firstNameError").innerText = "";
   }
 
-  if (errors.lastName) {
-    document.getElementById("lastNameError").innerHTML = errors.lastName;
+  if (errors.lastName) { // S'il y a des erreurs dans le nom, on affiche le texte, sinon on affiche rien
+    document.getElementById("lastNameError").innerText = errors.lastName;
   } else{
-    document.getElementById("lastNameError").innerHTML = "";
+    document.getElementById("lastNameError").innerText = "";
   } 
 
-  if (errors.email) {
-    document.getElementById("mailError").innerHTML = errors.email;
+  if (errors.email) { // S'il y a des erreurs dans le mail, on affiche le texte, sinon on affiche rien
+    document.getElementById("mailError").innerText = errors.email;
   } else{
-    document.getElementById("mailError").innerHTML = "";
+    document.getElementById("mailError").innerText = "";
   } 
 
-  if (errors.birthDate) { 
-    document.getElementById("birthdateError").innerHTML = errors.birthDate;
+  if (errors.birthDate) { // S'il y a des erreurs dans la date de naissance, on affiche le texte, sinon on affiche rien
+    document.getElementById("birthdateError").innerText = errors.birthDate;
   } else{
-    document.getElementById("birthdateError").innerHTML = "";
+    document.getElementById("birthdateError").innerText = "";
   }  
   
-  if (errors.quantity) {
-    document.getElementById("quantityError").innerHTML = errors.quantity;
+  if (errors.quantity) { // S'il y a des erreurs dans la quantité, on affiche le texte, sinon on affiche rien
+    document.getElementById("quantityError").innerText = errors.quantity;
   } else{
-    document.getElementById("quantityError").innerHTML = "";
+    document.getElementById("quantityError").innerText = "";
   }  
   
-  if (errors.checkBox) {
-    document.getElementById("check1Error").innerHTML = errors.checkBox;
+  if (errors.checkBox) { // Si les checbox villes ne sont pas cochées, on affiche le texte, sinon on affiche rien
+    document.getElementById("check1Error").innerText = errors.checkBox;
   } else{
-    document.getElementById("check1Error").innerHTML = "";
+    document.getElementById("check1Error").innerText = "";
   }  
   
-  if (errors.checkBox1) {
-    document.getElementById("check2Error").innerHTML = errors.checkBox1;
+  if (errors.checkBox1) { // Si les conditions d'utilisations ne sont pas cochées, on affiche le texte, sinon on affiche rien
+    document.getElementById("check2Error").innerText = errors.checkBox1;
   } else{
-    document.getElementById("check2Error").innerHTML = "";
+    document.getElementById("check2Error").innerText = "";
   }
   
 }
